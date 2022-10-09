713a8ffe367ba73f49a8c64f51da864adb46e5e3
sussa3007 <sussa1933@gmail.com>
2022-10-10 01:49:00 +0900

    Add #1 - api controllers

---
+++ src/main/java/com/toyproject/lineupproject/controller/api/APIPlaceController.java
@@ -0,0 +1,34 @@
+package com.toyproject.lineupproject.controller.api;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api")
+@RestController
+public class APIPlaceController {
+    @GetMapping("/places")
+    public List<String> getPlaces() {
+        return List.of("place1", "place2");
+    }
+
+    @PostMapping("/places")
+    public Boolean createPlace() {
+        return true;
+    }
+
+    @GetMapping("/places/{placeId}")
+    public String getPlace(@PathVariable Integer placeId) {
+        return "Place " + placeId;
+    }
+
+    @PutMapping("/places/{placeId}")
+    public Boolean modifyPlace(@PathVariable Integer placeId) {
+        return true;
+    }
+
+    @DeleteMapping("/places/{placeId}")
+    public Boolean removePlace(@PathVariable Integer placeId) {
+        return true;
+    }
+}
