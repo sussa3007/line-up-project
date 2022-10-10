49ae39cccbb00b90bae77f3afeb129fad7575c73
sussa3007 <sussa1933@gmail.com>
2022-10-10 15:40:55 +0900

    Update #1 -  admin view controller and view events tyhmleaf apply
    기존 admin view controller 구현 시 누락된 events view 업데이트

--- src/main/java/com/toyproject/lineupproject/controller/AdminController.java
+++ src/main/java/com/toyproject/lineupproject/controller/AdminController.java
@@ -1,16 +1,16 @@
 package com.toyproject.lineupproject.controller;
 
+import com.toyproject.lineupproject.constant.EventStatus;
 import com.toyproject.lineupproject.constant.PlaceType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 @RequestMapping("/admin")
 @Controller
@@ -33,8 +33,21 @@ public class AdminController {
         return "admin/place-detail";
     }
     @GetMapping("/events")
-    public String adminEvents(){
-        return "admin/events";
+    public ModelAndView adminEvents(
+            Integer placeId,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDatetime,
+            LocalDateTime eventEndDatetime
+    ) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("placeName", "place-" + placeId);
+        map.put("eventName", eventName);
+        map.put("eventStatus", eventStatus);
+        map.put("eventStartDatetime", eventStartDatetime);
+        map.put("eventEndDatetime", eventEndDatetime);
+
+        return new ModelAndView("admin/events", map);
     }
     @GetMapping("/events/{eventId}")
     public String adminEventDetail(@PathVariable Integer placeId){
