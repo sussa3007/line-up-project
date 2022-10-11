a29326fd749ab66051f01f9045eb173639537086
sussa3007 <sussa1933@gmail.com>
2022-10-12 01:19:11 +0900

    Implement #1 - APIEventController, dto Test
    mock 데이터로 임시 입출력 테스트 작성

--- src/main/java/com/toyproject/lineupproject/controller/api/APIEventController.java
+++ src/main/java/com/toyproject/lineupproject/controller/api/APIEventController.java
@@ -1,9 +1,16 @@
 package com.toyproject.lineupproject.controller.api;
 
+
+import com.toyproject.lineupproject.constant.EventStatus;
+import com.toyproject.lineupproject.dto.APIDataResponse;
+import com.toyproject.lineupproject.dto.EventRequest;
+import com.toyproject.lineupproject.dto.EventResponse;
 import com.toyproject.lineupproject.exception.GeneralException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequestMapping("/api")
@@ -11,30 +18,54 @@ import java.util.List;
 public class APIEventController {
 
     @GetMapping("/events")
-    public List<String> getEvents() throws Exception {
-        throw new HttpMediaTypeNotSupportedException("Test SPRING GET");
-//        return List.of("event1", "event2");
+    public APIDataResponse<List<EventResponse>> getEvents() {
+        return APIDataResponse.of(List.of(EventResponse.of(
+                1L,
+                "오후 운동",
+                EventStatus.OPENED,
+                LocalDateTime.of(2021, 1, 1, 13, 0, 0),
+                LocalDateTime.of(2021, 1, 1, 16, 0, 0),
+                0,
+                24,
+                "마스크 꼭 착용하세요"
+        )));
     }
 
     @PostMapping("/events")
-    public Boolean createEvent() {
-        throw new RuntimeException("Test Runtime");
-//        return true;
+    @ResponseStatus(HttpStatus.CREATED)
+    public APIDataResponse<Void> createEvent(@RequestBody EventRequest eventRequest) {
+        return APIDataResponse.empty();
     }
 
     @GetMapping("/events/{eventId}")
-    public String getEvent(@PathVariable Integer eventId) {
-        return "Event " + eventId;
+    public APIDataResponse<EventResponse> getEvent(@PathVariable Long eventId) {
+        if (eventId.equals(2L)) {
+            return APIDataResponse.empty();
+        }
+
+        return APIDataResponse.of(EventResponse.of(
+                1L,
+                "오후 운동",
+                EventStatus.OPENED,
+                LocalDateTime.of(2021, 1, 1, 13, 0, 0),
+                LocalDateTime.of(2021, 1, 1, 16, 0, 0),
+                0,
+                24,
+                "마스크 꼭 착용하세요"
+        ));
     }
 
     @PutMapping("/events/{eventId}")
-    public Boolean modifyEvent(@PathVariable Integer eventId) {
-        return true;
+    public APIDataResponse<Void> modifyEvent(
+            @PathVariable Long eventId,
+            @RequestBody EventRequest eventRequest
+    ) {
+        return APIDataResponse.empty();
     }
 
     @DeleteMapping("/events/{eventId}")
-    public Boolean removeEvent(@PathVariable Integer eventId) {
-        return true;
+    public APIDataResponse<Void> removeEvent(@PathVariable Long eventId) {
+        return APIDataResponse.empty();
     }
 
 
---
+++ src/main/java/com/toyproject/lineupproject/dto/EventResponse.java
@@ -0,0 +1,38 @@
+package com.toyproject.lineupproject.dto;
+
+import com.toyproject.lineupproject.constant.EventStatus;
+
+import java.time.LocalDateTime;
+
+public record EventResponse(
+        Long placeId,
+        String eventName,
+        EventStatus eventStatus,
+        LocalDateTime eventStartDatetime,
+        LocalDateTime eventEndDatetime,
+        Integer currentNumberOfPeople,
+        Integer capacity,
+        String memo
+) {
+    public static EventResponse of(
+            Long placeId,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDatetime,
+            LocalDateTime eventEndDatetime,
+            Integer currentNumberOfPeople,
+            Integer capacity,
+            String memo
+    ) {
+        return new EventResponse(
+                placeId,
+                eventName,
+                eventStatus,
+                eventStartDatetime,
+                eventEndDatetime,
+                currentNumberOfPeople,
+                capacity,
+                memo
+        );
+    }
+}
