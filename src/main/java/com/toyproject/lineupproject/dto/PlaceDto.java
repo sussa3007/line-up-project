05d32929d9725458e6983bb77faf964bfed1d08e
sussa3007 <sussa1933@gmail.com>
2022-10-30 16:51:20 +0900

    Impl #4 - PlaceService & PlaceController
    # Place Service, Controller 로직 , 테스트 코드 구현

--- src/main/java/com/toyproject/lineupproject/dto/PlaceDto.java
+++ src/main/java/com/toyproject/lineupproject/dto/PlaceDto.java
@@ -1,9 +1,12 @@
 package com.toyproject.lineupproject.dto;
 
 import com.toyproject.lineupproject.constant.PlaceType;
+import com.toyproject.lineupproject.domain.Place;
+
 import java.time.LocalDateTime;
 
 public record PlaceDto(
+        Long id,
         PlaceType placeType,
         String placeName,
         String address,
@@ -14,6 +17,7 @@ public record PlaceDto(
         LocalDateTime modifiedAt
 ) {
     public static PlaceDto of(
+            Long id,
             PlaceType placeType,
             String placeName,
             String address,
@@ -24,6 +28,7 @@ public record PlaceDto(
             LocalDateTime modifiedAt
     ) {
         return new PlaceDto(
+                id,
                 placeType,
                 placeName,
                 address,
@@ -33,4 +38,39 @@ public record PlaceDto(
                 createdAt,
                 modifiedAt);
     }
+    public static PlaceDto of(Place place){
+        return new PlaceDto(place.getId(),
+                place.getPlaceType(),
+                place.getPlaceName(),
+                place.getAddress(),
+                place.getPhoneNumber(),
+                place.getCapacity(),
+                place.getMemo(),
+                place.getCreatedAt(),
+                place.getModifiedAt()
+        );
+    }
+
+    public Place toEntity() {
+        return Place.of(
+                placeType,
+                placeName,
+                address,
+                phoneNumber,
+                capacity,
+                memo
+        );
+    }
+
+    public Place updateEntity(Place place) {
+        if (placeType != null) { place.setPlaceType(placeType); }
+        if (placeName != null) { place.setPlaceName(placeName); }
+        if (address != null) { place.setAddress(address); }
+        if (phoneNumber != null) { place.setPhoneNumber(phoneNumber); }
+        if (capacity != null) { place.setCapacity(capacity); }
+        if (memo != null) { place.setMemo(memo); }
+
+        return place;
+    }
+
 }
--- src/main/java/com/toyproject/lineupproject/service/PlaceService.java
+++ src/main/java/com/toyproject/lineupproject/service/PlaceService.java
@@ -1,4 +1,80 @@
 package com.toyproject.lineupproject.service;
 
+import com.querydsl.core.types.Predicate;
+import com.toyproject.lineupproject.constant.ErrorCode;
+import com.toyproject.lineupproject.dto.PlaceDto;
+import com.toyproject.lineupproject.exception.GeneralException;
+import com.toyproject.lineupproject.repository.PlaceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@RequiredArgsConstructor
 public class PlaceService {
+
+    private final PlaceRepository placeRepository;
+
+    // 전체조회
+    public List<PlaceDto> getPlaces(Predicate predicate) {
+        try {
+            return StreamSupport.stream(
+                    placeRepository.findAll(predicate).spliterator(),
+                            false)
+                    .map(PlaceDto::of)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR);
+        }
+    }
+    // 단건조회
+    public Optional<PlaceDto> getPlace(Long placeId) {
+        try {
+            return placeRepository.findById(placeId).map(PlaceDto::of);
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR);
+        }
+    }
+    // 만들기
+    public boolean createPlace(PlaceDto placeDto) {
+        try {
+            if(placeDto==null){ return false; }
+            placeRepository.save(placeDto.toEntity());
+            return true;
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR);
+        }
+    }
+    // 수정
+    public boolean modifyPlace(Long placeId, PlaceDto placeDto) {
+        try {
+            if(placeId==null||placeDto==null){
+                return false;
+            }
+            placeRepository.findById(placeId)
+                    .ifPresent(
+                            place -> placeRepository.save(placeDto.updateEntity(place)
+                            ));
+            return true;
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR);
+        }
+    }
+
+    // 삭제
+    public boolean removePlace(Long placeId) {
+        try {
+            if (placeId == null) {
+                return false;
+            }
+            placeRepository.deleteById(placeId);
+            return true;
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR);
+        }
+    }
 }
