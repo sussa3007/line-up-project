f91389c5c85a4244336bff89a1c827edac7e29e2
sussa3007 <sussa1933@gmail.com>
2022-11-20 14:26:23 +0900

    Update
    # build test update

--- src/test/java/com/toyproject/lineupproject/LineUpProjectApplicationTests.java
+++ src/test/java/com/toyproject/lineupproject/LineUpProjectApplicationTests.java
@@ -1,13 +1,12 @@
 package com.toyproject.lineupproject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 @Deprecated
 @SpringBootTest
 class LineUpProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
