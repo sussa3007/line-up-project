8b0aff305575a507ea900e6ad1507c588645f487
sussa3007 <sussa1933@gmail.com>
2022-11-28 14:45:04 +0900

    Impl #7 - JWT Implement
    # JWT 토큰 발급을 위한 Utils 클래스 정의

---
+++ src/main/java/com/toyproject/lineupproject/auth/jwt/utils/JwtProperties.java
@@ -0,0 +1,11 @@
+package com.toyproject.lineupproject.auth.jwt.utils;
+
+
+public class JwtProperties {
+
+    public static final int EXPIRATION_TIME = 600000; // 10분
+
+    public static final String COOKIE_NAME_ACCESS_TOKEN = "JWT-AUTHENTICATION_ACCESS";
+    public static final String COOKIE_NAME_REFRESH_TOKEN = "JWT-AUTHENTICATION_REFRESH";
+    public static final String REDIRECTION_URI = "REDIRECTION-URI";
+}
