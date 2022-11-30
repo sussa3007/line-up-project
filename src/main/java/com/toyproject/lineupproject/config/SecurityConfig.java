2bca91af70e475ae62a29ff34a195e2d30853f71
sussa3007 <sussa1933@gmail.com>
2022-12-01 00:42:52 +0900

    Impl #11 - OAuth2 Apply
    # 구글, 카카오 로그인 적용

--- src/main/java/com/toyproject/lineupproject/config/SecurityConfig.java
+++ src/main/java/com/toyproject/lineupproject/config/SecurityConfig.java
@@ -10,7 +10,13 @@ import com.toyproject.lineupproject.auth.jwt.handler.JwtAuthenticationEntryPoint
 import com.toyproject.lineupproject.auth.jwt.utils.CookieUtils;
 import com.toyproject.lineupproject.auth.jwt.utils.JwtAuthorityUtils;
 import com.toyproject.lineupproject.auth.jwt.utils.JwtProperties;
+import com.toyproject.lineupproject.auth.oauth2.OAuth2UserSuccessHandler;
+import com.toyproject.lineupproject.auth.oauth2.provider.CustomOAuth2Provider;
+import com.toyproject.lineupproject.auth.oauth2.service.CustomOAuth2UserService;
+import com.toyproject.lineupproject.service.AdminService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
@@ -18,8 +24,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
@@ -27,10 +34,19 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Value("${GOOGLE_AOUTH2_SAMPLEPROJECT_SECRETKEY}")
+    private String clientSecret;
+
+    @Value("${KAKAO_AOUTH2_SECRETKEY}")
+    private String kakaoClientSecret;
 
     private final JwtTokenizer jwtTokenizer;
 
@@ -38,20 +54,25 @@ public class SecurityConfig {
 
     private final CookieUtils cookieUtils;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
+    private final AdminService adminService;
+
+    private final CustomOAuth2UserService oAuth2UserService;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/", "/error","/events/**", "/places/**","/new-signup","/sign-up","/auth/reissue-token")
+        http.headers().frameOptions().sameOrigin()
+                .and()
+                .authorizeRequests()
+                // All
+                .antMatchers("/", "/error","/alert", "/events/**", "/places/**","/new-signup", "/sign-up", "/auth/reissue-token", "/new-OAuth", "/oauth-info")
                 .permitAll()
                 .antMatchers("/logout")
-                .hasAnyRole("SUPERADMIN","ADMIN","USER")
-                .antMatchers("/info")
-                .hasAnyRole("SUPERADMIN","ADMIN","USER")
+                .hasAnyRole("SUPERADMIN", "ADMIN", "USER")
+                // Register
+                .antMatchers( "/info")
+                .hasAnyRole("SUPERADMIN", "ADMIN", "USER")
+                // Super Admin
                 .antMatchers(
                         "/admin/user-all",
                         "/admin/modify",
@@ -60,8 +81,9 @@ public class SecurityConfig {
                         "/admin/places-all",
                         "/admin/user-all")
                 .hasRole("SUPERADMIN")
-                .antMatchers("/admin/**").hasAnyRole("SUPERADMIN","ADMIN")
-                .anyRequest().hasAnyRole("SUPERADMIN","ADMIN")
+                // Admin
+                .antMatchers("/admin/**").hasAnyRole("SUPERADMIN", "ADMIN")
+                .anyRequest().permitAll()
                 .and()
                 .apply(new CustomFilterConfig())
                 .and()
@@ -75,7 +97,8 @@ public class SecurityConfig {
                 .defaultSuccessUrl("/events")
                 .permitAll()
                 .and()
-                .exceptionHandling().authenticationEntryPoint(
+                .exceptionHandling()
+                .authenticationEntryPoint(
                         new JwtAuthenticationEntryPoint(cookieUtils)
                 )
                 .accessDeniedHandler(new JwtAccessDeniedHandler(cookieUtils))
@@ -86,6 +109,12 @@ public class SecurityConfig {
                 .deleteCookies(JwtProperties.COOKIE_NAME_ACCESS_TOKEN)
                 .deleteCookies(JwtProperties.COOKIE_NAME_REFRESH_TOKEN)
                 .deleteCookies(JwtProperties.REDIRECTION_URI)
+                .and()
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(new OAuth2UserSuccessHandler(jwtTokenizer, adminService))
+                        .userInfoEndpoint().userService(oAuth2UserService)
+
+                )
 
         ;
         return http.build();
@@ -108,7 +137,7 @@ public class SecurityConfig {
             AuthenticationManager authenticationManager =
                     builder.getSharedObject(AuthenticationManager.class);
             JwtAuthenticationFilter jwtAuthenticationFilter =
-                    new JwtAuthenticationFilter(authenticationManager, jwtTokenizer,cookieUtils);
+                    new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
 
             jwtAuthenticationFilter.setAuthenticationSuccessHandler(
                     new CustomAuthenticationSuccessHandler(cookieUtils));
@@ -127,5 +156,42 @@ public class SecurityConfig {
 
         }
     }
+
+    private ClientRegistration clientRegistration(
+            OAuth2ClientProperties clientProperties,
+            String client
+    ) {
+        if("google".equals(client)) {
+            OAuth2ClientProperties.Registration google = clientProperties.getRegistration().get("google");
+            return CustomOAuth2Provider.GOOGLE
+                    .getBuilder(client)
+                    .clientId(google.getClientId())
+                    .clientSecret(clientSecret)
+                    .build();
+        }
+        if("kakao".equals(client)) {
+            OAuth2ClientProperties.Registration kakao = clientProperties.getRegistration().get("kakao");
+            return CustomOAuth2Provider.KAKAO
+                    .getBuilder(client)
+                    .clientId(kakao.getClientId())
+                    .clientSecret(kakaoClientSecret)
+//                    .jwkSetUri("temp")
+                    .build();
+        }
+        return null;
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository(
+            OAuth2ClientProperties oAuth2ClientProperties
+    ) {
+        List<ClientRegistration> registrations = oAuth2ClientProperties
+                .getRegistration().keySet().stream()
+                .map(client -> clientRegistration(oAuth2ClientProperties, client))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+        return new InMemoryClientRegistrationRepository(registrations);
+    }
+
 
 }
