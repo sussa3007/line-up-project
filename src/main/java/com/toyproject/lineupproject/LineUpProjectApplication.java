ba2d9d3a4089793b3e750a0d57e4fbd9ea74e77d
sussa3007 <sussa1933@gmail.com>
2022-12-08 01:38:24 +0900

    FIx #main - AWS PipeLine
    # Code Deploy까지 완료 되지만 서버에서 어플리케이션이 정상적으로 구동되지 않는이슈 해결 시도 #1

--- src/main/java/com/toyproject/lineupproject/LineUpProjectApplication.java
+++ src/main/java/com/toyproject/lineupproject/LineUpProjectApplication.java
@@ -3,11 +3,9 @@ package com.toyproject.lineupproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.PropertySource;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
-@PropertySource("classpath:/env.yml")
 public class LineUpProjectApplication {
 
 	public static void main(String[] args) {
