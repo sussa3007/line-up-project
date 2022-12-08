865b9742d85c58c67a63d4668ef0bc96eaef0ca1
sussa3007 <sussa1933@gmail.com>
2022-12-08 18:26:37 +0900

    FIx #main - AWS PipeLine
    # Code Deploy까지 완료 되지만 서버에서 어플리케이션이 정상적으로 구동되지 않는이슈 해결 시도 #7
    # 로컬에서 RDS로  정상 구동됨.
    # RDS로 연동중 encoding 이슈
    
    # server_start 스크립트 파일 문제 확인
    # 재확인을 위한 커밋

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -7,7 +7,7 @@ echo "> 현재 시간: $(date)" >> /home/ubuntu/deploy.log
 echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
 
 echo "> build 파일 복사" >> /home/ubuntu/deploy.log
-DEPLOY_PATH=/home/ubuntu/
+DEPLOY_PATH=/home/ubuntu/build
 cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/deploy.log
