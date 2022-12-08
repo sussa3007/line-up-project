6351e4fe753d0db6c91ca1e7b3a8ea0eb22d555b
sussa3007 <sussa1933@gmail.com>
2022-12-09 02:06:25 +0900

    Update #main - EC2 자동 배포
    #3

--- src/main/java/com/toyproject/lineupproject/LineUpProjectApplication.java
+++ src/main/java/com/toyproject/lineupproject/LineUpProjectApplication.java
@@ -3,10 +3,11 @@ package com.toyproject.lineupproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.PropertySource;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
-//@PropertySource("classpath:env.yml")
+@PropertySource("classpath:env.yml")
 public class LineUpProjectApplication {
 
 	public static void main(String[] args) {
